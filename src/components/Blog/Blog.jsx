89a9b9c4afb18c97f360b0d9f2580632ec5acf91
HUSNEJIDAN <119681826+HUSNEJIDAN@users.noreply.github.com>
2025-03-01 00:07:18 +0600

    not finish yet

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,32 @@
-
-
-const Blog = ({blog}) => {
+import { CiBookmark } from "react-icons/ci";
+const Blog = ({blog,handleBookmark}) => {
    
-    const {title} = blog;
+    const {title,cover_photo,author_img,author,reading_time,posted_date,hashtag} = blog;
     return (
-        <div>
-            <h2 className="text-3xl">Title:{title}</h2>
+        <div className="mx-10">
+            <img src={cover_photo} alt={`Cover picture of the title ${title}`}/>
+            <div className="flex justify-between my-3 items-center">
+
+                <div className="flex">
+                <img className="w-14 h-14 rounded-[60px]" src={author_img} alt="" />
+                <div className="ml-5">
+                <h3 className="text-2xl">{author}</h3>
+                <p>{posted_date}</p>
+                </div>
+                </div>
+                <div className="flex items-center gap-1">
+                <p>{reading_time} min read</p>
+                <button onClick={() => handleBookmark(blog)}><CiBookmark /></button>
+                </div>
+            </div>
+            <h2 className="text-3xl mb-5"><span className="text-blue-500">Title</span>: {title}</h2>
+            <p>
+                {
+                    hashtag.map((hash, idx) => <span key={idx} className="mr-3"><a href="">{hash}</a></span>)
+
+                }
+            </p>
+
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Blog from "../Blog/Blog";
 
 
-const Blogs = () => {
+const Blogs = ({handleBookmark}) => {
     const [blogs, setBlogs]=useState([]);
     useEffect(() => {
         fetch('blogs.json')
@@ -11,10 +11,10 @@ const Blogs = () => {
         .then (data => setBlogs(data))
     },[])
     return (
-        <div className="md:w-2/3">
+        <div>
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map (blog=> <Blog key={blog.id} blog={blog}></Blog>)
+                blogs.map (blog=> <Blog key={blog.id} blog={blog} handleBookmark={handleBookmark}></Blog>)
             }
         </div>
     );
